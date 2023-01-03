d16868c046cc7d695ef62738fd982a7ee76e8467
AboodXD <aboood40091@gmail.com>
2023-01-03 16:13:25 +0400

    map/CourseDataFile: Implement getBg2Data()

--- src/map/CourseData.cpp
+++ src/map/CourseData.cpp
@@ -1,5 +1,17 @@
 #include <map/CourseData.h>
 
+const DistantViewData* CourseDataFile::getBg2Data(u8 index) const
+{
+    const DistantViewData* dv_data = static_cast<const DistantViewData*>(mBlock[cBlock_DistantViewData]);
+    s32 dv_data_num = mBlockEntryNum[cBlock_DistantViewData];
+
+    for (s32 i = 0; i < dv_data_num; i++)
+        if (dv_data[i].id == index)
+            return &(dv_data[i]);
+
+    return nullptr;
+}
+
 CourseData* CourseData::sInstance = nullptr;
 
 CourseDataFile* CourseData::getFile(s32 index)
