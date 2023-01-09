f4ec580f69c78f0938440e0d18e5edd1bc7f4131
AboodXD <aboood40091@gmail.com>
2023-01-09 14:25:16 +0400

    system/ResMgr: Implement loadArchiveRes(Impl_)()

--- include/system/ResMgr.h
+++ include/system/ResMgr.h
@@ -1,12 +1,33 @@
 #pragma once
 
+#include <container/seadStrTreeMap.h>
 #include <heap/seadDisposer.h>
 #include <prim/seadSafeString.h>
 #include <resource/seadArchiveRes.h>
+#include <resource/seadSZSDecompressor.h>
 
 class ResMgr
 {
     SEAD_SINGLETON_DISPOSER(ResMgr)
+
+private:
+    class ResHolder : public sead::IDisposer
+    {
+    public:
+        ResHolder(const sead::SafeString& key, sead::ArchiveRes* archive);
+
+        virtual ~ResHolder()
+        {
+            ResMgr::instance()->remove(mKey);
+        }
+
+    private:
+        sead::FixedSafeString<32> mKey;
+        sead::ArchiveRes* mpArchiveRes;
+
+        friend class ResMgr;
+    };
+    static_assert(sizeof(ResHolder) == 0x40);
 
 public:
     ResMgr();
@@ -19,5 +40,14 @@ public:
     void* getFileFromArchiveRes(sead::ArchiveRes* archive, const sead::SafeString& filename, u32* length = nullptr) const;
 
 private:
+    static sead::ArchiveRes* loadArchiveResImpl_(const sead::SafeString& archive_path, sead::Heap* heap, sead::Decompressor* decompressor);
+    static sead::ArchiveRes* loadArchiveResImpl_(const sead::SafeString& archive_path, sead::Heap* heap);
+
     static void* getFileFromArchiveResImpl_(sead::ArchiveRes* archive, const sead::SafeString& filename, u32* length);
+
+private:
+    sead::ArchiveRes* mpCourseResPack;
+    sead::FixedStrTreeMap<32, ResHolder*, 256> mResHolderTreeMap;
+    sead::SZSDecompressor* mpSZSDecompressor;
 };
+static_assert(sizeof(ResMgr) == 0x442C, "ResMgr size mismatch");
--- src/system/ResMgr.cpp
+++ src/system/ResMgr.cpp
@@ -1,6 +1,90 @@
+#include <system/ChallengeResCacheMgr.h>
 #include <system/ResMgr.h>
 
+#include <filedevice/seadFileDeviceMgr.h>
+#include <resource/seadResourceMgr.h>
+#include <resource/seadSharcArchiveRes.h>
+
 ResMgr* ResMgr::sInstance = nullptr;
+
+bool ResMgr::loadArchiveRes(const sead::SafeString& key, const sead::SafeString& archive_path, sead::Heap* heap, bool decompress)
+{
+    if (ChallengeResCacheMgr::instance() != nullptr)
+        if (ChallengeResCacheMgr::instance()->isEnableCacheHeap())
+            heap = ChallengeResCacheMgr::instance()->getCacheHeap();
+
+    if (mResHolderTreeMap.find(key))
+        return true;
+
+    sead::ArchiveRes* archive;
+
+    if (decompress)
+        archive = loadArchiveResImpl_(archive_path, heap, mpSZSDecompressor);
+    else
+        archive = loadArchiveResImpl_(archive_path, heap);
+
+    if (archive == nullptr)
+        return false;
+
+    mResHolderTreeMap.insert(key, new (heap) ResHolder(key, archive));
+    return true;
+}
+
+sead::ArchiveRes* ResMgr::loadArchiveResImpl_(const sead::SafeString& archive_path, sead::Heap* heap, sead::Decompressor* decompressor)
+{
+    sead::DirectResourceFactory<sead::SharcArchiveRes> factory;
+
+    sead::Resource* resource = nullptr;
+    sead::FixedSafeString<260> path_no_drive;
+    sead::FileDevice* device = sead::FileDeviceMgr::instance()->findDeviceFromPath(archive_path, &path_no_drive);
+    if (device != nullptr)
+    {
+        sead::ResourceMgr::LoadArg arg;
+        arg.device = device;
+        arg.path = path_no_drive;
+        arg.instance_heap = heap;
+        arg.load_data_heap = heap;
+        arg.instance_alignment = 0x2000;
+        arg.load_data_alignment = 0x2000;
+        arg.factory = &factory;
+
+        resource = sead::ResourceMgr::instance()->tryLoad(arg, "sarc", decompressor);
+    }
+
+    if (resource == nullptr)
+        return nullptr;
+
+    sead::ArchiveRes* archive = sead::DynamicCast<sead::ArchiveRes>(resource);
+    DCFlushRangeNoSync(archive->mRawData, archive->mRawSize);
+    return archive;
+}
+
+sead::ArchiveRes* ResMgr::loadArchiveResImpl_(const sead::SafeString& archive_path, sead::Heap* heap)
+{
+    sead::DirectResourceFactory<sead::SharcArchiveRes> factory;
+
+    sead::Resource* resource = nullptr;
+    sead::FixedSafeString<260> path_no_drive;
+    sead::FileDevice* device = sead::FileDeviceMgr::instance()->findDeviceFromPath(archive_path, &path_no_drive);
+    if (device != nullptr)
+    {
+        sead::ResourceMgr::LoadArg arg;
+        arg.device = device;
+        arg.path = path_no_drive;
+        arg.instance_heap = heap;
+        arg.load_data_heap = heap;
+        arg.instance_alignment = 0x2000;
+        arg.load_data_alignment = 0x2000;
+        arg.factory = &factory;
+
+        resource = sead::ResourceMgr::instance()->tryLoadWithoutDecomp(arg);
+    }
+
+    if (resource == nullptr)
+        return nullptr;
+
+    return sead::DynamicCast<sead::ArchiveRes>(resource);
+}
 
 void* ResMgr::getFileFromArchiveRes(const sead::SafeString& key, const sead::SafeString& filename, u32* length) const
 {
@@ -15,4 +99,10 @@ void* ResMgr::getFileFromArchiveRes(sead::ArchiveRes* archive, const sead::SafeS
 void* ResMgr::getFileFromArchiveResImpl_(sead::ArchiveRes* archive, const sead::SafeString& filename, u32* length)
 {
     return archive->getFile(filename, length);
+}
+
+ResMgr::ResHolder::ResHolder(const sead::SafeString& key, sead::ArchiveRes* archive)
+    : mpArchiveRes(archive)
+{
+    mKey = key;
 }
