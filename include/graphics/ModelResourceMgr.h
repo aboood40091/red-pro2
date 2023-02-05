f0fd09e618c6ac2cbbaadf9ce11451ae57bc9743
AboodXD <aboood40091@gmail.com>
2023-02-05 10:05:52 +0400

    graphics/ModelResourceMgr: Implement loadResFile()
    Additionally, add ModelResourceHolder.

--- include/graphics/ModelResourceMgr.h
+++ include/graphics/ModelResourceMgr.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <container/seadStrTreeMap.h>
 #include <heap/seadDisposer.h>
 #include <prim/seadSafeString.h>
 
@@ -9,14 +10,36 @@ class ModelResourceMgr
 {
     SEAD_SINGLETON_DISPOSER(ModelResourceMgr)
 
+private:
+    class ModelResourceHolder : public sead::IDisposer
+    {
+    public:
+        ModelResourceHolder(const sead::SafeString& key, ModelResource* p_mdl_res);
+
+        virtual ~ModelResourceHolder()
+        {
+            if (ModelResourceMgr::instance() != nullptr)
+                ModelResourceMgr::instance()->remove(mKey);
+        }
+
+    private:
+        sead::FixedSafeString<32> mKey;
+        ModelResource* mpModelResource;
+
+        friend class ModelResourceMgr;
+    };
+    static_assert(sizeof(ModelResourceHolder) == 0x40);
+
 public:
     ModelResourceMgr();
 
     bool loadResFile(const sead::SafeString& key, const sead::SafeString& archive_name, sead::Heap* heap = nullptr);
     ModelResource* getResource(const sead::SafeString& key) const;
 
+    void remove(const sead::SafeString& key);
+
 private:
-    u32 mResTree[0x4414 / sizeof(u32)]; // TreeMap<ModelResource>
+    sead::FixedStrTreeMap<32, ModelResourceHolder*, 256> mResHolderTreeMap;
     u32 _4424;
 };
 static_assert(sizeof(ModelResourceMgr) == 0x4428, "ModelResourceMgr size mismatch");
--- src/graphics/ModelResourceMgr.cpp
+++ src/graphics/ModelResourceMgr.cpp
@@ -1,3 +1,25 @@
+#include <graphics/ModelResource.h>
 #include <graphics/ModelResourceMgr.h>
 
 ModelResourceMgr* ModelResourceMgr::sInstance = nullptr;
+
+bool ModelResourceMgr::loadResFile(const sead::SafeString& key, const sead::SafeString& archive_name, sead::Heap* heap)
+{
+    if (getResource(key) != nullptr)
+        return false;
+
+    ModelResource* p_mdl_res = new (heap) ModelResource();
+    if (p_mdl_res == nullptr)
+        return false;
+
+    p_mdl_res->load(archive_name, heap);
+
+    mResHolderTreeMap.insert(key, new (heap) ModelResourceHolder(key, p_mdl_res));
+    return true;
+}
+
+ModelResourceMgr::ModelResourceHolder::ModelResourceHolder(const sead::SafeString& key, ModelResource* p_mdl_res)
+    : mpModelResource(p_mdl_res)
+{
+    mKey = key;
+}
