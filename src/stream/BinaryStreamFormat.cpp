9bd4a78a0484764005fb551ea96792a81ad95c7f
AboodXD <aboood40091@gmail.com>
2023-06-19 21:30:30 +0400

    Suppress unused variable warning

--- src/stream/BinaryStreamFormat.cpp
+++ src/stream/BinaryStreamFormat.cpp
@@ -44,7 +44,7 @@ static inline u64 EndianToHost64(bool is_be, u64 value)
 u8 BinaryStreamFormat::readU8(StreamSrc* src, bool is_be)
 {
     u8 ret = 0;
-    u32 rb = src->read(&ret, sizeof(u8));
+    [[maybe_unused]] u32 rb = src->read(&ret, sizeof(u8));
     RIO_ASSERT(rb == sizeof( u8 ));
     //
     return ret;
@@ -53,7 +53,7 @@ u8 BinaryStreamFormat::readU8(StreamSrc* src, bool is_be)
 u16 BinaryStreamFormat::readU16(StreamSrc* src, bool is_be)
 {
     u16 ret = 0;
-    u32 rb = src->read(&ret, sizeof(u16));
+    [[maybe_unused]] u32 rb = src->read(&ret, sizeof(u16));
     RIO_ASSERT(rb == sizeof( u16 ));
     ret = EndianToHost16(is_be, ret);
     return ret;
@@ -62,7 +62,7 @@ u16 BinaryStreamFormat::readU16(StreamSrc* src, bool is_be)
 u32 BinaryStreamFormat::readU32(StreamSrc* src, bool is_be)
 {
     u32 ret = 0;
-    u32 rb = src->read(&ret, sizeof(u32));
+    [[maybe_unused]] u32 rb = src->read(&ret, sizeof(u32));
     RIO_ASSERT(rb == sizeof( u32 ));
     ret = EndianToHost32(is_be, ret);
     return ret;
@@ -71,7 +71,7 @@ u32 BinaryStreamFormat::readU32(StreamSrc* src, bool is_be)
 u64 BinaryStreamFormat::readU64(StreamSrc* src, bool is_be)
 {
     u64 ret = 0;
-    u32 rb = src->read(&ret, sizeof(u64));
+    [[maybe_unused]] u32 rb = src->read(&ret, sizeof(u64));
     RIO_ASSERT(rb == sizeof( u64 ));
     ret = EndianToHost64(is_be, ret);
     return ret;
@@ -80,7 +80,7 @@ u64 BinaryStreamFormat::readU64(StreamSrc* src, bool is_be)
 s8 BinaryStreamFormat::readS8(StreamSrc* src, bool is_be)
 {
     s8 ret = 0;
-    u32 rb = src->read(&ret, sizeof(s8));
+    [[maybe_unused]] u32 rb = src->read(&ret, sizeof(s8));
     RIO_ASSERT(rb == sizeof( s8 ));
     //
     return ret;
@@ -89,7 +89,7 @@ s8 BinaryStreamFormat::readS8(StreamSrc* src, bool is_be)
 s16 BinaryStreamFormat::readS16(StreamSrc* src, bool is_be)
 {
     s16 ret = 0;
-    u32 rb = src->read(&ret, sizeof(s16));
+    [[maybe_unused]] u32 rb = src->read(&ret, sizeof(s16));
     RIO_ASSERT(rb == sizeof( s16 ));
     ret = EndianToHost16(is_be, ret);
     return ret;
@@ -98,7 +98,7 @@ s16 BinaryStreamFormat::readS16(StreamSrc* src, bool is_be)
 s32 BinaryStreamFormat::readS32(StreamSrc* src, bool is_be)
 {
     s32 ret = 0;
-    u32 rb = src->read(&ret, sizeof(s32));
+    [[maybe_unused]] u32 rb = src->read(&ret, sizeof(s32));
     RIO_ASSERT(rb == sizeof( s32 ));
     ret = EndianToHost32(is_be, ret);
     return ret;
@@ -107,7 +107,7 @@ s32 BinaryStreamFormat::readS32(StreamSrc* src, bool is_be)
 s64 BinaryStreamFormat::readS64(StreamSrc* src, bool is_be)
 {
     s64 ret = 0;
-    u32 rb = src->read(&ret, sizeof(s64));
+    [[maybe_unused]] u32 rb = src->read(&ret, sizeof(s64));
     RIO_ASSERT(rb == sizeof( s64 ));
     ret = EndianToHost64(is_be, ret);
     return ret;
@@ -116,7 +116,7 @@ s64 BinaryStreamFormat::readS64(StreamSrc* src, bool is_be)
 f32 BinaryStreamFormat::readF32(StreamSrc* src, bool is_be)
 {
     u32 ret = 0;
-    u32 rb = src->read(&ret, sizeof(u32));
+    [[maybe_unused]] u32 rb = src->read(&ret, sizeof(u32));
     RIO_ASSERT(rb == sizeof( u32 ));
     ret = EndianToHost32(is_be, ret);
     return *(f32*)&ret;
@@ -126,7 +126,7 @@ void BinaryStreamFormat::readBit(StreamSrc* src, void* data, u32 bitnum)
 {
     u8* data8 = static_cast<u8*>(data);
     u32 bytenum = bitnum / 8;
-    u32 rb = src->read(data8, bytenum);
+    [[maybe_unused]] u32 rb = src->read(data8, bytenum);
     RIO_ASSERT(rb == bytenum);
     bitnum -= bytenum * 8;
     if (bitnum > 0)
@@ -150,7 +150,7 @@ void BinaryStreamFormat::readString(StreamSrc* src, std::string* dst, u32 size)
         RIO_LOG("Warning: not enough buffer length. drop %u char(s).\n", remain_size);
     }
     dst->resize(size); dst->c_str(); // Forces null termination
-    u32 rb = src->read(dst->data(), size);
+    [[maybe_unused]] u32 rb = src->read(dst->data(), size);
     RIO_ASSERT(rb == size);
     if (size + 1 < dst->capacity())
     {
@@ -176,63 +176,63 @@ u32 BinaryStreamFormat::readMemBlock(StreamSrc* src, void* dst, u32 size)
 void BinaryStreamFormat::writeU8(StreamSrc* src, bool is_be, u8 value)
 {
     //
-    u32 wb = src->write(&value, sizeof(u8));
+    [[maybe_unused]] u32 wb = src->write(&value, sizeof(u8));
     RIO_ASSERT(wb == sizeof( u8 ));
 }
 
 void BinaryStreamFormat::writeU16(StreamSrc* src, bool is_be, u16 value)
 {
     value = EndianToHost16(is_be, value);
-    u32 wb = src->write(&value, sizeof(u16));
+    [[maybe_unused]] u32 wb = src->write(&value, sizeof(u16));
     RIO_ASSERT(wb == sizeof( u16 ));
 }
 
 void BinaryStreamFormat::writeU32(StreamSrc* src, bool is_be, u32 value)
 {
     value = EndianToHost32(is_be, value);
-    u32 wb = src->write(&value, sizeof(u32));
+    [[maybe_unused]] u32 wb = src->write(&value, sizeof(u32));
     RIO_ASSERT(wb == sizeof( u32 ));
 }
 
 void BinaryStreamFormat::writeU64(StreamSrc* src, bool is_be, u64 value)
 {
     value = EndianToHost64(is_be, value);
-    u32 wb = src->write(&value, sizeof(u64));
+    [[maybe_unused]] u32 wb = src->write(&value, sizeof(u64));
     RIO_ASSERT(wb == sizeof( u64 ));
 }
 
 void BinaryStreamFormat::writeS8(StreamSrc* src, bool is_be, s8 value)
 {
     //
-    u32 wb = src->write(&value, sizeof(s8));
+    [[maybe_unused]] u32 wb = src->write(&value, sizeof(s8));
     RIO_ASSERT(wb == sizeof( s8 ));
 }
 
 void BinaryStreamFormat::writeS16(StreamSrc* src, bool is_be, s16 value)
 {
     value = EndianToHost16(is_be, value);
-    u32 wb = src->write(&value, sizeof(s16));
+    [[maybe_unused]] u32 wb = src->write(&value, sizeof(s16));
     RIO_ASSERT(wb == sizeof( s16 ));
 }
 
 void BinaryStreamFormat::writeS32(StreamSrc* src, bool is_be, s32 value)
 {
     value = EndianToHost32(is_be, value);
-    u32 wb = src->write(&value, sizeof(s32));
+    [[maybe_unused]] u32 wb = src->write(&value, sizeof(s32));
     RIO_ASSERT(wb == sizeof( s32 ));
 }
 
 void BinaryStreamFormat::writeS64(StreamSrc* src, bool is_be, s64 value)
 {
     value = EndianToHost64(is_be, value);
-    u32 wb = src->write(&value, sizeof(s64));
+    [[maybe_unused]] u32 wb = src->write(&value, sizeof(s64));
     RIO_ASSERT(wb == sizeof( s64 ));
 }
 
 void BinaryStreamFormat::writeF32(StreamSrc* src, bool is_be, f32 value)
 {
     u32 u_value = EndianToHost32(is_be, *(u32*)&value);
-    u32 wb = src->write(&u_value, sizeof(u32));
+    [[maybe_unused]] u32 wb = src->write(&u_value, sizeof(u32));
     RIO_ASSERT(wb == sizeof( u32 ));
 }
 
@@ -240,7 +240,7 @@ void BinaryStreamFormat::writeBit(StreamSrc* src, const void* data, u32 bitnum)
 {
     const u8* data8 = static_cast<const u8*>(const_cast<void*>(data));
     u8 bytenum = bitnum / 8;
-    u32 wb = src->write(data8, bytenum);
+    [[maybe_unused]] u32 wb = src->write(data8, bytenum);
     RIO_ASSERT(wb == bytenum);
     bitnum -= bytenum * 8;
     if (bitnum > 0)
@@ -257,7 +257,7 @@ void BinaryStreamFormat::writeString(StreamSrc* src, const std::string& str, u32
     {
         str_size = size;
     }
-    u32 wb = src->write(str.c_str(), str_size);
+    [[maybe_unused]] u32 wb = src->write(str.c_str(), str_size);
     RIO_ASSERT(wb == str_size);
     char nullchar = '\0';
     for (; str_size < size; str_size++)
@@ -269,7 +269,7 @@ void BinaryStreamFormat::writeString(StreamSrc* src, const std::string& str, u32
 
 void BinaryStreamFormat::writeMemBlock(StreamSrc* src, const void* data, u32 size)
 {
-    u32 wb = src->write(data, size);
+    [[maybe_unused]] u32 wb = src->write(data, size);
     RIO_ASSERT(wb == size);
 }
 
