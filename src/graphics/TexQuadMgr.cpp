e44edd62f8fc5a9ca3210e4e24856e03e38d26a9
AboodXD <aboood40091@gmail.com>
2023-07-14 17:05:18 +0400

    graphics/TexQuadMgr: Implement initializeShader()

---
+++ src/graphics/TexQuadMgr.cpp
@@ -0,0 +1,48 @@
+#include <graphics/TexQuadMgr.h>
+
+void TexQuadMgr::initializeShader(agl::ShaderProgramArchive* p_archive, sead::Heap* heap)
+{
+    const s32 c_shader_num = 8;
+    const s32 c_uniform_num = 3;
+    const s32 c_sampler_num = 5;
+
+    static const char* s_shader_name[c_shader_num] = {
+        "texQuadCommon",
+        "texQuadWaveNormal",
+        "texQuadWaveSand",
+        "texQuadWavePoison",
+        "texQuadWaveMagmaWave",
+        "texQuadFloor",
+        "texQuadFlower",
+        "texQuadWaveNormalBehind"
+    };
+
+    static const char* s_uniform_name[c_uniform_num] = {
+        "cWorldMtx[0]",
+        "cViewMtx[0]",
+        "cProjMtx[0]"
+    };
+
+    static const char* s_sampler_name[c_sampler_num] = {
+        "sampler2d_00",
+        "sampler2d_01",
+        "sampler2d_normal",
+        "sampler2d_lightmap",
+        "sampler2d_lightmap2"
+    };
+
+    for (s32 i = 0; i < c_shader_num; i++)
+    {
+        const char* const name = s_shader_name[i];
+        s32 program_index = p_archive->searchShaderProgramIndex(name);
+        agl::ShaderProgram* p = const_cast<agl::ShaderProgram*>(p_archive->searchShaderProgram(program_index));
+
+        p->createUniform(c_uniform_num, heap);
+        for (s32 j = 0; j < c_uniform_num; j++)
+            p->setUniformName(j, s_uniform_name[j]);
+
+        p->createSamplerLocation(c_sampler_num, heap);
+        for (s32 j = 0; j < c_sampler_num; j++)
+            p->setSamplerLocationName(j, s_sampler_name[j]);
+    }
+}
