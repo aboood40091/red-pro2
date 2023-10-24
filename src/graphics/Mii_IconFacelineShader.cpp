a5e0611e216c0f723d166cca20af78cf68d40972
AboodXD <aboood40091@gmail.com>
2023-10-24 13:33:17 +0400

    graphics/Mii: Decompile entirety of IconFacelineShader

--- src/graphics/Mii_IconFacelineShader.cpp
+++ src/graphics/Mii_IconFacelineShader.cpp
@@ -1,13 +1,11 @@
 #include <graphics/Mii_IconFacelineShader.h>
+#include <graphics/ShaderHolder.h>
 
 namespace Mii {
 
 namespace {
 
-static const s32 c_uniform_num = 6;
-static const s32 c_sampler_num = 2;
-
-static const char* s_uniform_name[c_uniform_num] = {
+static const char* s_uniform_name[IconFacelineShader::cUniform_Num] = {
     "cWorldMtx[0]",
     "cViewMtx[0]",
     "cProjMtx[0]",
@@ -16,24 +14,72 @@ static const char* s_uniform_name[c_uniform_num] = {
     "cLineWidth"
 };
 
-static const char* s_sampler_name[c_sampler_num] = {
+static const char* s_sampler_name[IconFacelineShader::cSampler_Num] = {
     "sampler_depth",
     "sampler_color"
 };
 
 }
 
+IconFacelineShader::IconFacelineShader()
+    : mpShaderProgram(nullptr)
+{
+}
+
 void IconFacelineShader::initializeShader(agl::ShaderProgramArchive* p_archive, sead::Heap* heap)
 {
     agl::ShaderProgram* p = const_cast<agl::ShaderProgram*>(p_archive->searchShaderProgram("iconFaceline"));
 
-    p->createUniform(c_uniform_num, heap);
-    for (s32 j = 0; j < c_uniform_num; j++)
+    p->createUniform(cUniform_Num, heap);
+    for (s32 j = 0; j < cUniform_Num; j++)
         p->setUniformName(j, s_uniform_name[j]);
 
-    p->createSamplerLocation(c_sampler_num, heap);
-    for (s32 j = 0; j < c_sampler_num; j++)
+    p->createSamplerLocation(cSampler_Num, heap);
+    for (s32 j = 0; j < cSampler_Num; j++)
         p->setSamplerLocationName(j, s_sampler_name[j]);
+}
+
+bool IconFacelineShader::initialize()
+{
+    return initializeShader_();
+}
+
+void IconFacelineShader::destroy()
+{
+}
+
+void IconFacelineShader::setViewUniform(const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, const sead::Matrix34f& world_mtx) const
+{
+    mpShaderProgram->getUniformLocation(cUniform_ViewMtx).setUniform(view_mtx);
+    mpShaderProgram->getUniformLocation(cUniform_ProjMtx).setUniform(proj_mtx);
+    mpShaderProgram->getUniformLocation(cUniform_WorldMtx).setUniform(world_mtx);
+}
+
+void IconFacelineShader::setResolutionUniform(u32 width, u32 height) const
+{
+    f32 resolution_x = width;
+    mpShaderProgram->getUniformLocation(cUniform_ResolutionX).setUniform(resolution_x);
+    f32 resolution_y = height;
+    mpShaderProgram->getUniformLocation(cUniform_ResolutionY).setUniform(resolution_y);
+}
+
+void IconFacelineShader::setLineWidthUniform(s32 line_width) const
+{
+    f32 line_width_f = line_width;
+    mpShaderProgram->getUniformLocation(cUniform_LineWidth).setUniform(line_width_f);
+}
+
+bool IconFacelineShader::activateTexture(const agl::TextureData& texture_data, Sampler sampler)
+{
+    mTextureSampler[sampler].applyTextureData(texture_data);
+    return mTextureSampler[sampler].activate(mpShaderProgram, sampler);
+}
+
+bool IconFacelineShader::initializeShader_()
+{
+    mpShaderArchive = ShaderHolder::instance()->getShaderArchive("mii_icon_faceline_shader");
+    mpShaderProgram = mpShaderArchive->searchShaderProgram("iconFaceline");
+    return true;
 }
 
 }
