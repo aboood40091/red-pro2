587f0762410d61c229cf399325e72c39d6abe986
AboodXD <aboood40091@gmail.com>
2024-01-25 00:29:06 +0400

    graphics/GfxParameter: Implement getAgl{Env|Lmap}Binary() & getFileFromPack_()

--- src/graphics/GfxParameter.cpp
+++ src/graphics/GfxParameter.cpp
@@ -6,3 +6,30 @@ void GfxParameter::setDelegateForParameter(agl::utl::IParameterIO* p_param)
 {
     p_param->setDelegate(mDelegateData);
 }
+
+void* GfxParameter::getAglEnvBinary(const sead::SafeString& name, sead::Heap* heap) const
+{
+    return getFileFromPack_(
+        sead::FormatFixedSafeString<260>(
+            "%s.baglenv",
+            name.cstr()
+        ),
+        heap
+    );
+}
+
+void* GfxParameter::getAglLmapBinary(const sead::SafeString& name, sead::Heap* heap) const
+{
+    return getFileFromPack_(
+        sead::FormatFixedSafeString<260>(
+            "%s.bagllmap",
+            name.cstr()
+        ),
+        heap
+    );
+}
+
+void* GfxParameter::getFileFromPack_(const sead::SafeString& filename, sead::Heap* heap) const
+{
+    return mpPackArc->getFile(filename);
+}
