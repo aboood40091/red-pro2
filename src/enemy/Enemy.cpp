d86c906bc96142ff184c107b4acdf5714fc1a389
AboodXD <aboood40091@gmail.com>
2024-07-02 18:47:57 +0400

    enemy/Enemy: Implement changeState()
    Additionally, update headers submodule.

---
+++ src/enemy/Enemy.cpp
@@ -0,0 +1,14 @@
+#include <enemy/Enemy.h>
+
+void Enemy::changeState(const StateID& state_id)
+{
+    if (mStateMgr.getStateMethod().isSubStateMethod())
+    {
+        mStateMgr.getStateMethod().returnToMainStateMethod();
+        mStateMgr.getStateMethod().replaceStateMethod(state_id);
+    }
+    else
+    {
+        mStateMgr.getStateMethod().changeStateMethod(state_id);
+    }
+}
