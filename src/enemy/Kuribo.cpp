c13de6cba0d7083d53b3d953ab21175048e5f2d0
AboodXD <aboood40091@gmail.com>
2025-03-26 12:17:10 +0400

    enemy/Kuribo: Decompile ctor

---
+++ src/enemy/Kuribo.cpp
@@ -0,0 +1,18 @@
+#include <enemy/Kuribo.h>
+
+Kuribo::Kuribo(const ActorCreateParam& param)
+    : KuriboBase(param)
+    , mKuriboType(0)
+    , mKakiboAnmType(0)
+    , _1a96(0)
+    , _1a97(0)
+    , _1a98(false)
+    , _1a9c(0.0f, 0.0f, 0.0f)
+    , _1aa8(0)
+    , _1aac(0)
+    , _1ab0(false)
+    , _1ab4(0.0f)
+    , mGroundPosY(0.0f)
+    , _1abc(0)
+{
+}
