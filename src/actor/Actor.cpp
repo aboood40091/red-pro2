03cb23877b476bba92efc3bee97a2a6a74f0992f
AboodXD <aboood40091@gmail.com>
2025-08-06 19:18:19 +0400

    actor/Actor: Decompile clrComboCnt & incComboCnt

--- src/actor/Actor.cpp
+++ src/actor/Actor.cpp
@@ -1,7 +1,10 @@
 #include <actor/Actor.h>
 #include <collision/ActorCollisionCheckMgr.h>
+#include <game/SubjectMgr.h>
 #include <player/PlayerMgr.h>
 #include <player/PlayerObject.h>
+
+#define COMBO_CNT_MAX 9
 
 void Actor::removeCollisionCheck()
 {
@@ -28,5 +31,17 @@ bool Actor::checkCarried(s32* p_player_no)
     if (p_player_no != nullptr)
         *p_player_no = -1;
     return false;
+}
+
+void Actor::clrComboCnt()
+{
+    mComboCnt = 0;
+    SubjectMgr::instance()->onAcComboCntClr();
+}
+
+void Actor::incComboCnt()
+{
+    mComboCnt = sead::Mathi::min(mComboCnt + 1, COMBO_CNT_MAX - 1);
+    SubjectMgr::instance()->onComboCntInc(mComboCnt, SubjectMgr::cComoType_AcCombo);
 }
 
