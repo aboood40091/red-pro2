d21742a7696f9e8a8376d92bb6f94c14a4369ee9
AboodXD <aboood40091@gmail.com>
2025-08-06 19:17:39 +0400

    actor/Actor: Decompile checkCarried

--- src/actor/Actor.cpp
+++ src/actor/Actor.cpp
@@ -1,5 +1,7 @@
 #include <actor/Actor.h>
 #include <collision/ActorCollisionCheckMgr.h>
+#include <player/PlayerMgr.h>
+#include <player/PlayerObject.h>
 
 void Actor::removeCollisionCheck()
 {
@@ -9,5 +11,22 @@ void Actor::removeCollisionCheck()
 void Actor::reviveCollisionCheck()
 {
     ActorCollisionCheckMgr::instance()->entry(mCollisionCheck);
+}
+
+bool Actor::checkCarried(s32* p_player_no)
+{
+    for (s32 i = 0; i < 4; i++)
+    {
+        PlayerObject* p_player = PlayerMgr::instance()->getPlayerObject(i);
+        if (p_player != nullptr && p_player->getCarryActorID() == mActorUniqueID)
+        {
+            if (p_player_no != nullptr)
+                *p_player_no = p_player->getPlayerNo();
+            return true;
+        }
+    }
+    if (p_player_no != nullptr)
+        *p_player_no = -1;
+    return false;
 }
 
