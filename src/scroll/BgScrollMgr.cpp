f98b3787f4449666348722f8b83120d3435f5aa4
AboodXD <aboood40091@gmail.com>
2025-12-12 20:32:40 +0400

    Update headers submodule, with subsequent changes

--- src/scroll/BgScrollMgr.cpp
+++ src/scroll/BgScrollMgr.cpp
@@ -8,6 +8,6 @@ f32 BgScrollMgr::getBgCenterYPos() const
     // This makes it so that the BG Center Y position is always at the center of the screen when the player is standing at the bottom of the area
     // (Respecting the initial zoom)
 
-    f32 zoom_multiplier = mTrackingMgr->getCurrentZoomMultiplier();
-    return AreaTask::instance()->getBound().getMin().y + zoom_multiplier * 224 - zoom_multiplier * 224 * 0.5f;
+    f32 zoom = mTrackingMgr->getZoomTargetMin();
+    return AreaTask::instance()->getBound().getMin().y + zoom * 224 - zoom * 224 * 0.5f;
 }
